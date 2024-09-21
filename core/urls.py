e1dae7b3920fbac2dbd9580ed30ee23a530bff77
batyrnurkhan <makyzhanbatyr@gmail.com>
2024-09-22 02:13:44 +0500

    added api/ to swagger (with domain)

--- core/urls.py
+++ core/urls.py
@@ -18,7 +18,7 @@ schema_view = get_schema_view(
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
-    url='/api/',
+    url='https://sport-resume.com/api/',  # Full base URL for API
 )
 
 urlpatterns = [
