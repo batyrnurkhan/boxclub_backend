9a8cd66679b1a3ad9d29007c5ab77d19b109a9bd
batyrnurkhan <makyzhanbatyr@gmail.com>
2024-09-23 13:57:59 +0500

    edit HomeView

--- core/views.py
+++ core/views.py
@@ -11,6 +11,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class HomeAPIView(APIView):
     @swagger_auto_schema(
         operation_description="Retrieve all news articles and a selection of verified users by weight category.",
@@ -20,25 +21,26 @@ class HomeAPIView(APIView):
                 examples={
                     "application/json": {
                         "latest_news": [
-                            {"id": 1, "title": "Latest Technology Advancements", "content": "Exploring the latest in tech..."},
+                            {"id": 1, "title": "Latest Technology Advancements",
+                             "content": "Exploring the latest in tech..."},
                             {"id": 2, "title": "Second News", "content": "Content of the second news..."}
                         ],
-                        "users_0_57": [
-                            {"username": "user1", "weight": 55},
-                            {"username": "user2", "weight": 57}
+                        "super_lightweight_fighters": [
+                            {"username": "user1", "weight": 125},
+                            {"username": "user2", "weight": 135}
                         ],
-                        "users_66_70": [
-                            {"username": "user3", "weight": 66},
-                            {"username": "user4", "weight": 68}
+                        "lightweight_fighters": [
+                            {"username": "user3", "weight": 155},
+                            {"username": "user4", "weight": 170}
                         ],
-                        "users_94_120": [
-                            {"username": "user5", "weight": 94},
-                            {"username": "user6", "weight": 120}
+                        "heavyweight_fighters": [
+                            {"username": "user5", "weight": 185},
+                            {"username": "user6", "weight": 205}
                         ],
                         "links": {
-                            "full_users_0_57": "http://example.com/accounts/search/?weight_min=0&weight_max=57",
-                            "full_users_66_70": "http://example.com/accounts/search/?weight_min=66&weight_max=70",
-                            "full_users_94_120": "http://example.com/accounts/search/?weight_min=94&weight_max=120"
+                            "full_super_lightweight_fighters": "http://example.com/accounts/search/?weight_min=0&weight_max=145",
+                            "full_lightweight_fighters": "http://example.com/accounts/search/?weight_min=146&weight_max=170",
+                            "full_heavyweight_fighters": "http://example.com/accounts/search/?weight_min=171"
                         }
                     }
                 }
@@ -51,12 +53,19 @@ class HomeAPIView(APIView):
         news_serializer = NewsSerializer(all_news, many=True)
 
         # Helper function to get random verified users by weight range
-        def get_users_by_weight(min_weight, max_weight):
-            eligible_users = CustomUser.objects.filter(
-                is_verified=True,
-                profile__weight__gte=min_weight,
-                profile__weight__lte=max_weight
-            ).prefetch_related('profile').distinct()
+        def get_users_by_weight(min_weight, max_weight=None):
+            if max_weight:
+                eligible_users = CustomUser.objects.filter(
+                    is_verified=True,
+                    profile__weight__gte=min_weight,
+                    profile__weight__lte=max_weight
+                ).prefetch_related('profile').distinct()
+            else:
+                eligible_users = CustomUser.objects.filter(
+                    is_verified=True,
+                    profile__weight__gte=min_weight
+                ).prefetch_related('profile').distinct()
+
             if eligible_users.exists():
                 random_users = random.sample(list(eligible_users), min(len(eligible_users), 4))
                 serialized_data = VerifiedUserProfileSerializer(random_users, many=True).data
@@ -69,23 +78,23 @@ class HomeAPIView(APIView):
             else:
                 return []
 
-        # Collect data for all weight ranges
-        users_0_65 = get_users_by_weight(0, 65)
-        users_66_93 = get_users_by_weight(66, 93)
-        users_94_120 = get_users_by_weight(94, 120)
+        # Collect data for weight ranges
+        super_lightweight_fighters = get_users_by_weight(0, 145)  # Сверхлегкие бойцы
+        lightweight_fighters = get_users_by_weight(146, 170)  # Легкие бойцы
+        heavyweight_fighters = get_users_by_weight(171)  # Бойцы тяжеловесы
 
-        logger.info(f'Final users_0_65: {users_0_65}')
-        logger.info(f'Final users_66_93: {users_66_93}')
-        logger.info(f'Final users_94_120: {users_94_120}')
+        logger.info(f'Super lightweight fighters: {super_lightweight_fighters}')
+        logger.info(f'Lightweight fighters: {lightweight_fighters}')
+        logger.info(f'Heavyweight fighters: {heavyweight_fighters}')
 
         return Response({
             'latest_news': news_serializer.data,
-            'users_0_65': users_0_65,
-            'users_66_93': users_66_93,
-            'users_94_120': users_94_120,
+            'users_0_65': super_lightweight_fighters,
+            'users_66_93': lightweight_fighters,
+            'users_94_120': heavyweight_fighters,
             'links': {
-                'full_users_0_65': request.build_absolute_uri('/accounts/search/?weight_min=0&weight_max=65'),
-                'full_users_66_93': request.build_absolute_uri('/accounts/search/?weight_min=66&weight_max=93'),
-                'full_users_94_120': request.build_absolute_uri('/accounts/search/?weight_min=94&weight_max=120')
+                'full_users_0_65': request.build_absolute_uri('/accounts/search/?weight_min=0&weight_max=145'),
+                'full_users_66_93': request.build_absolute_uri('/accounts/search/?weight_min=146&weight_max=170'),
+                'full_users_94_120': request.build_absolute_uri('/accounts/search/?weight_min=171')
             }
         })
